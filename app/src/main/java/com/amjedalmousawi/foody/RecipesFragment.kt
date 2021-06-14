cbd6027234baa5adaf6601de6a0253036ef74857
amalm9479 <amalm9479@gmail.com>
2021-06-15 00:54:27 +0300

    4

--- app/src/main/java/com/amjedalmousawi/foody/RecipesFragment.kt
+++ app/src/main/java/com/amjedalmousawi/foody/RecipesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
 
@@ -14,8 +15,8 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        view.recyclerview.showShimmer()
+        return view
              }
-
-
 }

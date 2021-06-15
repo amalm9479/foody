9dde6db9ec2da3805c18deac4b5146289302580b
amalm9479 <amalm9479@gmail.com>
2021-06-15 13:33:03 +0300

    5

---
+++ app/src/main/java/com/amjedalmousawi/foody/models/FoodRecipe.kt
@@ -0,0 +1,9 @@
+package com.amjedalmousawi.foody.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class FoodRecipe(
+    @SerializedName("results")
+    val results: List<Result>
+)
